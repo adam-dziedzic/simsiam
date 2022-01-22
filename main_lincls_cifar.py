46b46fd7996be6038dea8cab52713fac195a15ac
Adam Dziedzic <adam.cajf@gmail.com>
2022-01-22 09:03:03 -0500

    added cifar100 dataset

--- main_lincls_cifar.py
+++ main_lincls_cifar.py
@@ -360,6 +360,37 @@ def main_worker(gpu, ngpus_per_node, args):
         classes = ('plane', 'car', 'bird', 'cat', 'deer',
                    'dog', 'frog', 'horse', 'ship', 'truck')
 
+    elif args.dataset_name == 'cifar100':
+        transform_train = transforms.Compose([
+            transforms.Resize(224),
+            # transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize(224),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        train_dataset = datasets.CIFAR100(
+            root=f'{prefix}/home/{user}/data/cifar100', train=True,
+            download=True, transform=transform_train)
+        trainloader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=args.batch_size, shuffle=True,
+            num_workers=args.workers)
+
+        test_dataset = datasets.CIFAR100(
+            root=f'{prefix}/home/{user}/data/cifar100', train=False,
+            download=True, transform=transform_test)
+        val_loader = torch.utils.data.DataLoader(
+            test_dataset, batch_size=args.batch_size, shuffle=False,
+            num_workers=args.workers)
+
     elif args.dataset_name == 'svhn':
 
         transform_svhn = transforms.Compose([
